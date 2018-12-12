41ad07e0925e9c7fe35377ee4a1fde7110e494d9
Steven Carr <stevencarr@utexas.edu>
2018-12-11 18:16:01 -0600

    Updates

--- toyexample.py
+++ toyexample.py
@@ -9,44 +9,72 @@ class UTM_Network():
         self.n_towers = n_towers
         # State space and transtions for single agent
         self.states = []
+        self.state_labels = []
         self.transitions = set()
         self.rewards = dict()
         self.actlist = set()
         id_nos = 0
         for loc in tower_locs:
             self.states.append(Tower(loc, 1, 2, id_nos))
+            self.state_labels.append(id_nos)
             id_nos += 1
         for i in self.states:
             for j in self.states:
                 if i.DistanceTo(j) < i.VLOS_radius + j.VLOS_radius:
                     if i.id_no is j.id_no:
                         i.AddAction(j.id_no, 1)
-                        self.actlist.union(set(i.actlist.keys()))
+                        self.actlist = self.actlist.union(set(i.actlist.keys()))
                         self.transitions.add((i.id_no, j.id_no, j.id_no, 1))
-                        self.rewards.update({(i.id_no, j.id_no): 1.5})
+                        self.rewards.update({(i.id_no, j.id_no,j.id_no): -1.5})
                     else:
                         i.AddAction(j.id_no, prob_success)
-                        self.actlist.union(set(i.actlist.keys()))
+                        self.actlist = self.actlist.union(set(i.actlist.keys()))
                         self.transitions.add((i.id_no, j.id_no, j.id_no, prob_success))
                         self.transitions.add((i.id_no, j.id_no, i.id_no, 1 - prob_success))
-                        self.rewards.update({(i.id_no, j.id_no): i.DistanceTo(j)})
+                        self.rewards.update({(i.id_no, j.id_no,j.id_no): -i.DistanceTo(j)})
+                        self.rewards.update({(i.id_no, j.id_no,i.id_no): -1.5})
         for s in self.states:
             co_ord_sector = set()
-            co_ord_sector.union(set(s.actlist.keys()))
+            co_ord_sector = co_ord_sector.union(set(s.actlist.keys()))
             for k in co_ord_sector:
-                co_ord_sector.union(set(k.actlist.keys()))
+                co_ord_sector = co_ord_sector.union(set(self.states[k].actlist.keys()))
+            s.AddCoordinationZones(co_ord_sector)
 
         # Agents
         self.n_UAVs = len(initial)
         self.UAV = []
         for init, tar in zip(initial, tasks):
             self.UAV.append(UAV(init, tar))
-            self.UAV[-1].AddMDP(self.states, self.actlist, self.transitions)
-            self.UAV[-1].Policy = self.CalculateRoute(self.UAV[-1].mdp, self.rewards, self.UAV[-1].target)
+            self.UAV[-1].AddMDP(self.state_labels, self.actlist, self.transitions)
+            self.UAV[-1].rewards = self.UAV[-1].AddRewards(self.rewards,self.UAV[-1].target,self.states)
+            _,self.UAV[-1].policy,self.UAV[-1].Q = self.CalculateRoute(self.UAV[-1].mdp, self.UAV[-1].rewards,tar)
+        self.UTM_Goal = self.UAVsAtGoal(tasks)
 
-    def CalculateRoute(self, mdp, R, target):
-        return mdp.E_step_value_iteration(R, set(), target, 0.1, 0.8)
+    def UAVsAtGoal(self,tasks):
+        flag = True
+        for uav,tar in zip(self.UAV,tasks):
+            flag = flag and (uav.current_state is tar)
+        return flag
 
+    def CalculateRoute(self, mdp, R,target):
+        return mdp.E_step_value_iteration(R,set(),set([target]))
+
+    def Propagate(self,tasks):
+        for u_av in self.UAV:
+            u_av.PropagateUAV(u_av.policy)
+        self.UTM_Goal = self.UAVsAtGoal(tasks)
+
+    def DisplayStates(self):
+        print([u_av.current_state for u_av in self.UAV])
+
+
+    def Run(self,tasks):
+        while not self.UTM_Goal:
+            self.Propagate(tasks)
+            self.DisplayStates()
+
+    def Coordinate(self):
+        
 
 class Tower():
     def __init__(self, location, n_operators, VLOS_radius, id_no):
@@ -62,19 +90,44 @@ class Tower():
     def AddAction(self, state, prob):
         self.actlist.update({state: prob})
 
+    def AddCoordinationZones(self,co_ord):
+        self.co_ord_sector = co_ord
+
 
 class UAV():
     def __init__(self, initial, target):
         self.initial_loc = initial
         self.current_state = initial
         self.target = target
+        self.mdp = []
+        self.rewards = []
+        self.policy = dict()
 
-    def AddMDP(self, states, actlist, transitions):
-        self.mdp = MDP(states, actlist, transitions)
+    def AddMDP(self, states,actlist, transitions):
+        self.mdp = MDP(states,actlist, transitions)
+
+    def AddRewards(self,R,target,states):
+        rewards = R.copy()
+        for s in states:
+            for a in s.actlist:
+                if a is target:
+                    s_next = a
+                    rewards[s.id_no,a,s_next] = 100
+        return rewards
 
 
-locations = [1, 8, 9]
-targets = [6, 5, 8]
+    def PropagateUAV(self,policy):
+        s = self.current_state
+        a = policy[s]
+        a = list(a)[0]
+        next_s,target = self.mdp.sample(s,a)
+        self.current_state = next_s
+
+locations = [0, 7, 8]
+targets = [5, 4, 7]
 tower_positions = [[0, 0], [1, 1], [1, 2.8], [2, 2], [1.3, -0.2], [4, 3], [2.8, 3.4], [3.1, 1.8], [2, 4]]
 
 network = UTM_Network(3, 9, tower_positions, locations, targets)
+network.DisplayStates()
+network.Run(targets)
+
